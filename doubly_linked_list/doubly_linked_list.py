9cfb4007bcbe8e4edf9ff8822e9d062656e2cd3d
Hunter Raffety <hsraffety@gmail.com>
2019-10-14 11:03:19 -0700

    end of lecture

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -92,17 +92,27 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -121,5 +131,26 @@ class DoublyLinkedList:
         else:
             node.delete()
     """Returns the highest value currently in the list"""
+    # def get_max(self):
+    #     if not self.head:
+    #         return None
+        
+    #     max_val = self.head.value
+    #     current = self.head
+
+    #     while current:
+    #         if current.value > max_val:
+    #             mav_val = current.value
+    #         current = current.next
+    #     return max_val
+
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        max_val = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_val:
+                max_val = current.value
+            current = current.next
+        return max_val
