8dd8bec027c88684e7ffc4c1bd91959719bdf7c9
Hunter Raffety <hsraffety@gmail.com>
2019-10-14 18:49:20 -0700

    didnt push. hooray

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,18 +2,38 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+"""
+Queue operates on a First In, First Out basis. Meaning, one end is used for insertion, and the other is used for the deletion
+
+Typically this will use at least two pointers. Queue always uses "enqueue" and "dequeue". Queue is the more complex version for shifting things around. Enqueuing adds something to the read and dequeuing pushes that item from the front. (Insert / Delete).
+
+The pointers need to have access to both the rear and front of the data list.
+
+Think of it like this for queue:
+
+A person is in line for movie tickets. The person in the front, will get a ticket first before the person in the back, who will receive theirs last.
+
+Time complexity: O(1) - constant.
+"""
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # time complexity > * (it's constant)
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        if self.len() > 0:
+            self.size -= 1
+            value = self.storage.tail.value
+            self.storage.remove_from_tail()
+            return value
 
     def len(self):
-        pass
+        return self.storage.__len__()
+
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,36 @@ from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
 
+"""
+Stack utilizes a Last In, First Out method. The same end is used to insert and delete elements. This makes the MOST sense to me because it is super similar to how JavaScript operates with the push and pop methods for arrays.
+
+This is the simpler implementation of getting things around in a list.
+
+Stack pushes the item to the top and pop pops the element off of the top of the stack. (Insert / Delete)
+
+Stack uses one pointer to reference the same place, because that's the place that push and pop are going to impact.
+
+Time complexity: O(1) - constant.
+
+"""
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # time complexity is constant.
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        if self.len() > 0:
+            self.size -= 1
+            value = self.storage.head.value
+            self.storage.remove_from_head()
+            return value
 
     def len(self):
-        pass
+        return self.storage.__len__()
